fefc96a1ee0bb940dd8fc8f15dad9da3ce12f90f
Guillermo <GuillermoX7@gmail.com>
2020-09-29 16:55:42 +0200

    Fixed multiplication.asm from examples

--- examples/multiplication.asm
+++ examples/multiplication.asm
@@ -4,12 +4,12 @@ main:   LDI R1, 3
         STOP
         
 mult:   LDI R0, 0
+        CPI R1, 0
+        BRZS end
         CPI R2, 0
-        BRZS retorna
-        CPI R2, 0
-        BRZS retorna
+        BRZS end
 loop:   ADD R0, R1
         SUBI R2, 1
-        BRZS retorna
-        JMP bucle
+        BRZS end
+        JMP loop
 end:    RET
