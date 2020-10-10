5dbbee8f0f4dd5df827967b986501950371ad992
Guillermo <GuillermoX7@gmail.com>
2020-10-10 20:24:13 +0200

    Added operations

--- src/cs2010/cs_op.c
+++ src/cs2010/cs_op.c
@@ -1,5 +1,7 @@
 /** @file cs_op.c */
 
+#include <stdbool.h>
+
 #include "cs_instructions.h"
 #include "cs_op.h"
 
@@ -37,71 +39,294 @@ void cs_op_ld_stepper(cs2010 *cs) {
 }
 
 void cs_op_ld_microstepper(cs2010 *cs) {
-
+    switch (cs->microop) {
+    case 0:
+        cs->reg.ac = *cs->reg.regfile[CS_GET_REG_B(cs->reg.ir)];
+        cs_microfetch(cs);
+        break;
+    case 1:
+        cs->reg.mar = cs->reg.ac;
+        cs_microfetch(cs);
+        break;
+    case 2:
+        cs->reg.mdr = cs->mem.ram[cs->reg.mar];
+        cs_microfetch(cs);
+        break;
+    case 3:
+    default:
+        *cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)] = cs->reg.mdr;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_sts_stepper(cs2010 *cs) {
-
+    cs->mem.ram[CS_GET_ARG_B(cs->reg.ir)] = *cs->reg.regfile[CS_GET_ARG_A(cs->reg.ir)];
+    cs_fetch(cs);
 }
 
 void cs_op_sts_microstepper(cs2010 *cs) {
-
+    switch (cs->microop) {
+    case 0:
+        cs->reg.ac = CS_GET_ARG_B(cs->reg.ir);
+        cs_microfetch(cs);
+        break;
+    case 1:
+        cs->reg.mar = cs->reg.ac;
+        cs->reg.ac = *cs->reg.regfile[CS_GET_ARG_A(cs->reg.ir)];
+        cs_microfetch(cs);
+        break;
+    case 2:
+        cs->reg.mdr = cs->reg.ac;
+        cs_microfetch(cs);
+        break;
+    case 3:
+    default:
+        cs->mem.ram[cs->reg.mar] = cs->reg.mdr;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_lds_stepper(cs2010 *cs) {
-
+    *cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)] = cs->mem.ram[CS_GET_ARG_B(cs->reg.ir)];
+    cs_fetch(cs);
 }
 
 void cs_op_lds_microstepper(cs2010 *cs) {
-
+    switch (cs->microop) {
+    case 0:
+        cs->reg.ac = CS_GET_ARG_B(cs->reg.ir);
+        cs_microfetch(cs);
+        break;
+    case 1:
+        cs->reg.mar = cs->reg.ac;
+        cs_microfetch(cs);
+        break;
+    case 2:
+        cs->reg.mdr = cs->mem.ram[cs->reg.mar];
+        cs_microfetch(cs);
+        break;
+    case 3:
+    default:
+        *cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)] = cs->reg.mdr;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_call_stepper(cs2010 *cs) {
-
+    cs->mem.ram[cs->reg.sp--] = cs->reg.pc;
+    cs->reg.pc = CS_GET_ARG_B(cs->reg.ir);
+    cs_fetch(cs);
 }
 
 void cs_op_call_microstepper(cs2010 *cs) {
-
+    switch (cs->microop) {
+    case 0:
+        cs->reg.mdr = cs->reg.pc;
+        cs->reg.ac = CS_GET_ARG_B(cs->reg.ir);
+        cs_microfetch(cs);
+        break;
+    case 1:
+        cs->reg.mar = cs->reg.sp--;
+        cs_microfetch(cs);
+        break;
+    case 2:
+    default:
+        cs->reg.pc = cs->reg.ac;
+        cs->mem.ram[cs->reg.mar] = cs->reg.mdr;
+        cs_microfetch(cs);
+        break;
+    }
 }
 
 void cs_op_ret_stepper(cs2010 *cs) {
-
+    cs->reg.pc = cs->mem.ram[++cs->reg.sp];
+    cs_fetch(cs);
 }
 
 void cs_op_ret_microstepper(cs2010 *cs) {
+    switch (cs->microop) {
+    case 0:
+        cs->reg.sp++;
+        cs_microfetch(cs);
+        break;
+    case 1:
+        cs->reg.mar = cs->reg.sp;
+        cs_microfetch(cs);
+        break;
+    case 2:
+        cs->reg.mdr = cs->mem.ram[cs->reg.mar];
+        cs_microfetch(cs);
+        break;
+    case 3:
+    default:
+        cs->reg.pc = cs->reg.mdr;
+        cs_fetch(cs);
+        break;
+    }
+}
 
+bool cs_op_check_jmp(cs2010 *cs) {
+    bool jump = false;
+    switch (CS_GET_JMP_CONDITION(cs->reg.ir)) {
+    case CS_JMP_COND_EQUAL:
+        jump = cs->reg.sr & CS_SR_Z;
+        break;
+    case CS_JMP_COND_LOWER:
+        jump = cs->reg.sr & CS_SR_C;
+        break;
+    case CS_JMP_COND_OVERFLOW:
+        jump = cs->reg.sr & CS_SR_V;
+        break;
+    case CS_JMP_COND_SLOWER:
+        jump = !!(cs->reg.sr & CS_SR_V) ^ !!(cs->reg.sr & CS_SR_N);
+        break;
+    }
+    return jump;
 }
 
 void cs_op_brxx_stepper(cs2010 *cs) {
-
+    if (cs_op_check_jmp(cs)) {
+        cs->reg.pc = CS_GET_ARG_B(cs->reg.ir);
+    }
+    cs_fetch(cs);
 }
 
 void cs_op_brxx_microstepper(cs2010 *cs) {
-
+    switch (cs->microop) {
+    case 0:
+        if (cs_op_check_jmp(cs)) {
+            cs->reg.ac = CS_GET_ARG_B(cs->reg.ir);
+            cs_microfetch(cs);
+        } else {
+            cs_fetch(cs);
+        }
+        break;
+    case 1:
+    default:
+        cs->reg.pc = cs->reg.ac;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_jmp_stepper(cs2010 *cs) {
-
+    cs->reg.pc = CS_GET_ARG_B(cs->reg.ir);
+    cs_fetch(cs);
 }
 
 void cs_op_jmp_microstepper(cs2010 *cs) {
-
+    switch (cs->microop) {
+    case 0:
+        cs->reg.ac = CS_GET_ARG_B(cs->reg.ir);
+        cs_microfetch(cs);
+        break;
+    case 1:
+    default:
+        cs->reg.pc = cs->reg.ac;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_add_stepper(cs2010 *cs) {
-
+    uint8_t *a = cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)];
+    uint8_t *b = cs->reg.regfile[CS_GET_REG_B(cs->reg.ir)];
+    uint8_t c = *a + *b;
+    uint8_t a_7 = *a >> 7;
+    uint8_t b_7 = *b >> 7;
+    uint8_t c_7 = c >> 7;
+    /* SR = 0000VNZC */
+    cs->reg.sr =
+        ((!(a_7 ^ b_7)) & (a_7 ^ c_7)) << 3 |   /* sign(a) == sign(b) AND sign(a) != sign(c) */
+        (c_7) << 2 |                            /* (c >> 7) << 2 */
+        (!c) << 1 |
+        (c < *a);
+    *a = c;
+    cs_fetch(cs);
 }
 
 void cs_op_add_microstepper(cs2010 *cs) {
+    uint8_t *a;
+    uint8_t *b;
+    uint8_t c;
+    uint8_t a_7;
+    uint8_t b_7;
+    uint8_t c_7;
 
+    switch (cs->microop) {
+    case 0:
+        a = cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)];
+        b = cs->reg.regfile[CS_GET_REG_B(cs->reg.ir)];
+        c = *a + *b;
+        a_7 = *a >> 7;
+        b_7 = *b >> 7;
+        c_7 = c >> 7;
+        /* SR = 0000VNZC */
+        cs->reg.sr =
+            ((!(a_7 ^ b_7)) & (a_7 ^ c_7)) << 3 |   /* sign(a) == sign(b) AND sign(a) != sign(c) */
+            (c_7) << 2 |                            /* (c >> 7) << 2 */
+            (!c) << 1 | 
+            (c < *a);
+        cs->reg.ac = c;
+        cs_microfetch(cs);
+    case 1:
+    default:
+        *cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)] = cs->reg.ac;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_sub_stepper(cs2010 *cs) {
-
+    uint8_t *a = cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)];
+    uint8_t *b = cs->reg.regfile[CS_GET_REG_B(cs->reg.ir)];
+    uint8_t c = *a - *b;
+    uint8_t a_7 = *a >> 7;
+    uint8_t b_7 = *b >> 7;
+    uint8_t c_7 = c >> 7;
+    /* SR = 0000VNZC */
+    cs->reg.sr =
+        ((!(a_7 ^ b_7)) & (a_7 ^ c_7)) << 3 |   /* sign(a) == sign(b) AND sign(a) != sign(c) */
+        (c_7) << 2 |                            /* (c >> 7) << 2 */
+        (!c) << 1 |
+        (c < *a);
+    *a = c;
+    cs_fetch(cs);
 }
 
 void cs_op_sub_microstepper(cs2010 *cs) {
+    uint8_t *a;
+    uint8_t *b;
+    uint8_t c;
+    uint8_t a_7;
+    uint8_t b_7;
+    uint8_t c_7;
 
+    switch (cs->microop) {
+    case 0:
+        a = cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)];
+        b = cs->reg.regfile[CS_GET_REG_B(cs->reg.ir)];
+        c = *a - *b;
+        a_7 = *a >> 7;
+        b_7 = *b >> 7;
+        c_7 = c >> 7;
+        /* SR = 0000VNZC */
+        cs->reg.sr =
+            ((!(a_7 ^ b_7)) & (a_7 ^ c_7)) << 3 |   /* sign(a) == sign(b) AND sign(a) != sign(c) */
+            (c_7) << 2 |                            /* (c >> 7) << 2 */
+            (!c) << 1 |
+            (c < *a);
+        cs->reg.ac = c;
+        cs_microfetch(cs);
+    case 1:
+    default:
+        *cs->reg.regfile[CS_GET_REG_A(cs->reg.ir)] = cs->reg.ac;
+        cs_fetch(cs);
+        break;
+    }
 }
 
 void cs_op_cp_stepper(cs2010 *cs) {
